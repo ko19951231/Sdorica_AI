90eab29bddcf76da7f3d0b49d65fbc91c8933e27
wanglolly <wanglolly@gmail.com>
2018-07-02 13:06:41 +0800

    add Feature class

---
+++ Feature.cpp
@@ -0,0 +1,57 @@
+#include "Feature.h"
+
+virtual void Feature::dump(const board& b, std::ostream& out = info) const {
+	out << b << "estimate = " << estimate(b) << std::endl;
+}
+
+friend std::ostream& Feature::operator <<(std::ostream& out, const feature& w) {
+	std::string name = w.name();
+	int len = name.length();
+	out.write(reinterpret_cast<char*>(&len), sizeof(int));
+	out.write(name.c_str(), len);
+	float* weight = w.weight;
+	size_t size = w.size();
+	out.write(reinterpret_cast<char*>(&size), sizeof(size_t));
+	out.write(reinterpret_cast<char*>(weight), sizeof(float) * size);
+	return out;
+}
+
+friend std::istream& Feature::operator >>(std::istream& in, feature& w) {
+	std::string name;
+	int len = 0;
+	in.read(reinterpret_cast<char*>(&len), sizeof(int));
+	name.resize(len);
+	in.read(&name[0], len);
+	if (name != w.name()) {
+		error << "unexpected feature: " << name << " (" << w.name() << " is expected)" << std::endl;
+		std::exit(1);
+	}
+	float* weight = w.weight;
+	size_t size;
+	in.read(reinterpret_cast<char*>(&size), sizeof(size_t));
+	if (size != w.size()) {
+		error << "unexpected feature size " << size << "for " << w.name();
+		error << " (" << w.size() << " is expected)" << std::endl;
+		std::exit(1);
+	}
+	in.read(reinterpret_cast<char*>(weight), sizeof(float) * size);
+	if (!in) {
+		error << "unexpected end of binary" << std::endl;
+		std::exit(1);
+	}
+	return in;
+}
+
+static float* Feature::alloc(size_t num) {
+	static size_t total = 0;
+	static size_t limit = (1 << 30) / sizeof(float); // 1G memory
+	try {
+		total += num;
+		if (total > limit) throw std::bad_alloc();
+		return new float[num]();
+	} catch (std::bad_alloc&) {
+		error << "memory limit exceeded" << std::endl;
+		std::exit(-1);
+	}
+	return nullptr;
+}
---
+++ Feature.h
@@ -0,0 +1,50 @@
+#ifndef FEATURE_H
+#define FEATURE_H
+#include<cstdio>
+#include<cstdlib>
+
+class Feature {
+public:
+	Feature(size_t len) : length(len), weight(alloc(len)) {}
+	Feature(Feature&& f) : length(f.length), weight(f.weight) { f.weight = nullptr; }
+	Feature(const Feature& f) = delete;
+	Feature& operator =(const Feature& f) = delete;
+	virtual ~Feature() { delete[] weight; }
+
+	float& operator[] (size_t i) { return weight[i]; }
+	float operator[] (size_t i) const { return weight[i]; }
+	size_t size() const { return length; }
+
+public: // should be implemented
+
+	/**
+	 * estimate the value of a given board
+	 */
+	virtual float estimate(const board& b) const = 0;
+	/**
+	 * update the value of a given board, and return its updated value
+	 */
+	virtual float update(const board& b, float u) = 0;
+	/**
+	 * get the name of this feature
+	 */
+	virtual std::string name() const = 0;
+
+public:
+
+	/**
+	 * dump the detail of weight table of a given board
+	 */
+	virtual void dump(const board& b, std::ostream& out = info) const;
+
+	friend std::ostream& operator <<(std::ostream& out, const feature& w);
+
+	friend std::istream& operator >>(std::istream& in, feature& w);
+
+protected:
+	static float* alloc(size_t num);
+	size_t length;
+	float* weight;
+};
+
+#endif
