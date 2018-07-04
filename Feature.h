60c3297f118bce8860ed50cdafd78ffd2701b14d
wanglolly <wanglolly@gmail.com>
2018-07-04 21:35:05 +0800

    debug

--- Feature.h
+++ Feature.h
@@ -82,7 +82,10 @@ public:
 	}
 
 	float* alloc(size_t num) {
+		size_t total = 0;
+		size_t limit = (1 << 30) / sizeof(float); // 1G memory
 		try {
+			total += 1 << num;
 			if (total > limit) throw std::bad_alloc();
 			return new float[(1 << num)]();
 		} catch (std::bad_alloc&) {
