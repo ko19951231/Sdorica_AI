bc79b418d7af98ddd9e197d6a53f634e22d732f7
wanglolly <wanglolly@gmail.com>
2018-07-05 21:31:49 +0800

    debug

--- Feature.h
+++ Feature.h
@@ -12,7 +12,7 @@ public:
 		length = 22;
 		weight = new float[(1 << 26)];
 		for(int i = 0 ; i < (1 << 26) ; i++){
-			weight[i] = rand()%100;
+			weight[i] = 0;
 		}
 	}
 	virtual ~Feature() { delete[] weight; }
