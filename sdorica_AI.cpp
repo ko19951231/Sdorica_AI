28cb449f287e6c39bad6ae728f855ae0c91eb56d
wanglolly <wanglolly@gmail.com>
2018-07-10 18:16:00 +0800

    delete random tiles

--- sdorica_AI.cpp
+++ sdorica_AI.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main()
 {
     //Some parameter that often change
-    int num_episode = 100000;
+    int num_episode = 1000;
     string load_weight = "Models/Sdorica.tar";
     string save_weight = "Models/Sdorica.tar";
     bool load = false;
@@ -93,11 +93,6 @@ int main()
                         maximum = est + rew;
                     }
                 }
-            }
-            //random
-            if((i % 1000) == 0){
-                best_slide = rand()%next_move.size();
-                best_object = rand()%3;
             }
             //Assign to the real one
             vector<int> r=next_move[best_slide].r;
